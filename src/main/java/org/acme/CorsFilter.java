6cd36d1f9502497fe6c0967979f71c1811c1f9b8
Invictum-Fitness <info@invictum-fitness.com>
2026-06-23 18:35:01 +0200

    fix: remove allowed credentials to allow cross origins

--- src/main/java/org/acme/CorsFilter.java
+++ src/main/java/org/acme/CorsFilter.java
@@ -15,6 +15,5 @@ public class CorsFilter implements ContainerResponseFilter {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
     }
 }
