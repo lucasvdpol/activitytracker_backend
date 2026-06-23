9e760fc65c4a4e943df06a9e8c4ee0d15c43ead8
Invictum-Fitness <info@invictum-fitness.com>
2026-06-23 20:24:16 +0200

    fix: frontend url in cors

--- src/main/java/org/acme/CorsFilter.java
+++ src/main/java/org/acme/CorsFilter.java
@@ -8,12 +8,16 @@ import java.io.IOException;
 
 @Provider
 public class CorsFilter implements ContainerResponseFilter {
-
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://activity.lucasvandepol.com");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
+
+        if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
+            responseContext.setStatus(200);
+        }
     }
 }
